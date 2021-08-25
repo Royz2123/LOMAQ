4b4a462d00fbae9cfb0ecdaed7022ce1d2699544
Royz2123 <1Qazwsxdc>
2021-08-25 04:14:35 +0300

    yas

--- scripts/linux/run_multi_particle.sh
+++ scripts/linux/run_multi_particle.sh
@@ -3,8 +3,8 @@
 export DISPLAY=localhost:0.0
 LANG=en_US
 
-xvfb-run -e /dev/stdout python3 src/main.py --config=local_qmix,qmix --env-config=multi_particle
-xvfb-run -e /dev/stdout python3 src/plot.py --config=local_qmix,qmix --env-config=multi_particle
+xvfb-run -e /dev/stdout python3 src/main.py --config=local_qmix,qmix,iql_local,iql,qtran --env-config=multi_particle
+xvfb-run -e /dev/stdout python3 src/plot.py --config=local_qmix,qmix,iql_local,iql,qtran --env-config=multi_particle
 
 #python3 src/main.py --config=local_qmix,qmix --env-config=multi_particle
 #python3 src/plot.py --config=local_qmix,qmix --env-config=multi_particle
