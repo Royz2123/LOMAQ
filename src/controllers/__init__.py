be182350698b09d9252f834a810f0910107765c6
Royz2123 <1Qazwsxdc>
2021-11-05 18:27:00 +0200

    Added test with more comparisons

--- src/controllers/__init__.py
+++ src/controllers/__init__.py
@@ -1,7 +1,10 @@
+
 REGISTRY = {}
 
+from .central_basic_controller import CentralBasicMAC
 from .basic_controller import BasicMAC
 from .hetro_controller import HetroMac
 
 REGISTRY["basic_mac"] = BasicMAC
 REGISTRY["hetro_mac"] = HetroMac
+REGISTRY["basic_central_mac"] = CentralBasicMAC
--- src/learners/__init__.py
+++ src/learners/__init__.py
@@ -1,6 +1,7 @@
 from .q_learner import QLearner
 from .coma_learner import COMALearner
 from .qtran_learner import QLearner as QTranLearner
+from .max_q_learner import MAXQLearner
 
 REGISTRY = {}
 
@@ -8,3 +9,4 @@ REGISTRY = {}
 REGISTRY["q_learner"] = QLearner
 REGISTRY["coma_learner"] = COMALearner
 REGISTRY["qtran_learner"] = QTranLearner
+REGISTRY["max_q_learner"] = MAXQLearner
